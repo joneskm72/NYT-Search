c4ebd282b77f7622dd0429c28a91ccc708c26442
Kasey Jones <joneskm72@gmail.com>
2020-07-28 20:25:24 -0400

    Updated JS code

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+//css
+
 $(".jumbotron").css({"background-color": "navy", "color": "white"});
 $(".container").css({"background-color": "navy", "text-align": "center","color": "white"});
 $(".btn-primary").css({"background-color": "transparent", "color": "black", "border": "transparent"});
+
+
+var searchT = "";
+var searchNum = 1;
+var searchyear = 2020;
+$("#search-button").on("click", function() {
+    var searchValue = $("#searchTerm").val();
+    searchT = searchValue;
+    console.log(searchValue);
+    searchNum = $("#searchNum").val();
+    console.log(searchNum);
+    searchyear = $("#searchYear").val();
+    console.log(searchyear);
+});
